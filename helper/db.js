b0d32495a26fa2c3b28bbd1c4ead6ff2b4aa1134
Berkay MIZRAK <mizrakby@gmail.com>
2022-08-18 12:00:35 +0300

    connected to db using with mongoose

---
+++ helper/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    // mongoose.connect('mongodb://USERNAME:PASSWORD@berkaymizrak.com:2222/nfc_card');
+    mongoose.connect('mongodb://localhost/nfc_card');
+    mongoose.connection.on('open', () => {
+        console.log('MongoDB connection opened');
+    }).on('error', (err) => {
+        console.log(err);
+    }).on('disconnected', () => {
+        console.log('MongoDB disconnected');
+    }).on('close', () => {
+        console.log('MongoDB closed');
+    }).on('reconnected', () => {
+        console.log('MongoDB reconnected');
+    }).on('connected', () => {
+        console.log('MongoDB connected');
+    });
+}
