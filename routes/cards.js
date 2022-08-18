a1923b3ee0e2c7185125126246dd056269a9fdff
Berkay MIZRAK <mizrakby@gmail.com>
2022-08-19 01:06:30 +0300

    crud done for cards

--- routes/cards.js
+++ routes/cards.js
@@ -6,7 +6,10 @@ const Card = require('../models/Card');
 
 /* GET cards listing. */
 router.get('/', (req, res, next) => {
-    res.send({status: 'success', message: 'Welcome to the cards API'});
+    Card.find((err, cards) => {
+        if (err) return next({message: 'Error fetching cards.', detailed_message: err.message});
+        res.json({status: 'success', message: 'Cards retrieved successfully', count: cards.length, data: cards});
+    }).sort({createdAt: -1});
 });
 
 router.post('/', (req, res, next) => {
@@ -16,19 +19,34 @@ router.post('/', (req, res, next) => {
         name: name,
         icon: icon
     });
-    card.save((err, data) => {
+    card.save((err, card) => {
         if (err) {
             return res.status(500).send({status: 'error', message: err.message});
         }
-        return res.status(200).send({status: 'success', message: 'Card saved successfully', data: data});
+        return res.status(200).send({status: 'success', message: 'Card saved successfully', data: card});
     });
-    /*const promise = card.save();
-    promise.then(data => {
-        return res.status(200).send({status: 'success', message: 'Card saved successfully', data: data});
+});
+
+router.get('/:id', (req, res, next) => {
+    Card.findById(req.params.id, (err, card) => {
+        // if (err) return res.json({status: 'error', message: 'Card not found.', detailed_message: err});
+        if (err) return next({message: 'Card not found.', detailed_message: err.message});
+        res.json({status: 'success', message: 'Card retrieved successfully', data: card});
     });
-    promise.catch(err => {
-        return res.status(500).send({status: 'error', message: err.message});
-    });*/
+});
+
+router.put('/:id', (req, res, next) => {
+    Card.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, card) => {
+        if (err) return next({message: 'Error updating card.', detailed_message: err.message});
+        res.json({status: 'success', message: 'Card updated successfully', data: card});
+    }).select('-__v');
+});
+
+router.delete('/:id', (req, res, next) => {
+    Card.findByIdAndRemove(req.params.id, (err, card) => {
+        if (err) return next({message: 'Error deleting card.', detailed_message: err.message});
+        res.json({status: 'success', message: 'Card deleted successfully', data: card});
+    }).select('-__v');
 });
 
 module.exports = router;
