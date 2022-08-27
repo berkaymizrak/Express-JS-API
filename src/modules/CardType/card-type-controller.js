c0b1b4b26ef38025a515de726c822f0a3ad7e1c8
Berkay MIZRAK <mizrakby@gmail.com>
2022-08-28 02:03:05 +0300

    card type controller divided into queries

--- src/modules/CardType/card-type-controller.js
+++ src/modules/CardType/card-type-controller.js
@@ -1,107 +1,28 @@
 // Models
 import CardType from './card-type-model.js';
+import {
+    cardTypeDeleteQuery,
+    cardTypeGetQuery,
+    cardTypeCreateQuery,
+    cardTypesListQuery,
+} from './card-type-query.js';
 
 const listCardTypes = async (req, res, next) => {
-    await CardType.find({}, { __v: 0 })
-        .sort({ createdAt: -1 })
-        .then(data => {
-            return res.status(200).send({
-                success: true,
-                message: 'Card Types retrieved successfully',
-                count: data.length,
-                data,
-            });
-        })
-        .catch(err => {
-            return next({
-                status: 500,
-                success: false,
-                message: 'Error fetching card types',
-                detailed_message: err.message,
-            });
-        });
+    return next(await cardTypesListQuery());
 };
 
 const createCardType = async (req, res, next) => {
-    const { name, icon, base_url } = req.body;
-    const data = new CardType({
-        name,
-        icon,
-        base_url,
-    });
-    await data
-        .save()
-        .then(data => {
-            return res.status(200).send({
-                success: true,
-                message: 'Card Type created successfully',
-                data,
-            });
-        })
-        .catch(err => {
-            return next({
-                status: 500,
-                success: false,
-                message: 'Error creating card type',
-                detailed_message: err.message,
-            });
-        });
+    return next(await cardTypeCreateQuery(req.body));
 };
 
 const getCardType = async (req, res, next) => {
     const { id } = req.params;
-    await CardType.findById({ _id: id }, { __v: 0 })
-        .then(data => {
-            if (!data) {
-                return res.status(200).send({
-                    success: false,
-                    message: 'Card type not found',
-                    data,
-                });
-            } else {
-                return res.status(200).send({
-                    success: true,
-                    message: 'Card type retrieved successfully',
-                    data,
-                });
-            }
-        })
-        .catch(err => {
-            return next({
-                status: 500,
-                success: false,
-                message: 'Error fetching card type',
-                detailed_message: err.message,
-            });
-        });
+    return next(await cardTypeGetQuery(id));
 };
 
 const deleteCardType = async (req, res, next) => {
     const { id } = req.params;
-    await CardType.findOneAndDelete(id)
-        .then(data => {
-            if (!data) {
-                return res.status(200).send({
-                    success: false,
-                    message: 'Card type not found',
-                    data,
-                });
-            } else {
-                return res.status(200).send({
-                    success: true,
-                    message: 'Card type deleted successfully',
-                    data,
-                });
-            }
-        })
-        .catch(err => {
-            return next({
-                status: 500,
-                success: false,
-                message: 'Error deleting card type',
-                detailed_message: err.message,
-            });
-        });
+    return next(await cardTypeDeleteQuery(id));
 };
 
 export { listCardTypes, createCardType, getCardType, deleteCardType };
---
+++ src/modules/CardType/card-type-query.js
@@ -0,0 +1,110 @@
+// Models
+import CardType from './card-type-model.js';
+
+const cardTypesListQuery = async () => {
+    return await CardType.find({}, { __v: 0 })
+        .sort({ createdAt: -1 })
+        .then(data => {
+            return {
+                status: 200,
+                success: true,
+                message: 'Card Types retrieved successfully',
+                count: data.length,
+                data,
+            };
+        })
+        .catch(err => {
+            return {
+                status: 500,
+                success: false,
+                message: 'Error fetching card types',
+                detailed_message: err.message,
+            };
+        });
+};
+
+const cardTypeCreateQuery = async body => {
+    const { name, icon, base_url } = body;
+    return await new CardType({
+        name,
+        icon,
+        base_url,
+    })
+        .save()
+        .then(data => {
+            return {
+                status: 200,
+                success: true,
+                message: 'Card Type created successfully',
+                data,
+            };
+        })
+        .catch(err => {
+            return {
+                status: 500,
+                success: false,
+                message: 'Error creating card type',
+                detailed_message: err.message,
+            };
+        });
+};
+
+const cardTypeGetQuery = async id => {
+    return await CardType.findById({ _id: id }, { __v: 0 })
+        .then(data => {
+            if (!data) {
+                return {
+                    status: 200,
+                    success: false,
+                    message: 'Card type not found',
+                    data,
+                };
+            } else {
+                return {
+                    status: 200,
+                    success: true,
+                    message: 'Card type retrieved successfully',
+                    data,
+                };
+            }
+        })
+        .catch(err => {
+            return {
+                status: 500,
+                success: false,
+                message: 'Error fetching card type',
+                detailed_message: err.message,
+            };
+        });
+};
+
+const cardTypeDeleteQuery = async id => {
+    return await CardType.findOneAndDelete(id)
+        .then(data => {
+            if (!data) {
+                return {
+                    status: 200,
+                    success: false,
+                    message: 'Card type not found',
+                    data,
+                };
+            } else {
+                return {
+                    status: 200,
+                    success: true,
+                    message: 'Card type deleted successfully',
+                    data,
+                };
+            }
+        })
+        .catch(err => {
+            return {
+                status: 500,
+                success: false,
+                message: 'Error deleting card type',
+                detailed_message: err.message,
+            };
+        });
+};
+
+export { cardTypeDeleteQuery, cardTypeGetQuery, cardTypeCreateQuery, cardTypesListQuery };
