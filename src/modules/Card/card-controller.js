67606d9e48ec71329a2e0321df69543764bdf6bf
Berkay MIZRAK <mizrakby@gmail.com>
2022-08-28 01:55:50 +0300

    card controller divided into queries

--- src/modules/Card/card-controller.js
+++ src/modules/Card/card-controller.js
@@ -1,168 +1,33 @@
 // Models
 import Card from './card-model.js';
+import {
+    cardCreateQuery,
+    cardDeleteQuery,
+    cardGetQuery,
+    cardListDetailedQuery,
+    cardListQuery,
+} from './card-query.js';
 
 const listCards = async (req, res, next) => {
-    await Card.find({}, { __v: 0 })
-        .sort({ createdAt: -1 })
-        .then(data => {
-            return res.status(200).send({
-                success: true,
-                message: 'Cards retrieved successfully',
-                count: data.length,
-                data,
-            });
-        })
-        .catch(err => {
-            return next({
-                status: 500,
-                success: false,
-                message: 'Error fetching cards',
-                detailed_message: err.message,
-            });
-        });
+    return next(await cardListQuery());
 };
 
 const listDetailedCards = async (req, res, next) => {
-    await Card.aggregate([
-        {
-            $lookup: {
-                from: 'cardtypes',
-                localField: 'card_type_id',
-                foreignField: '_id',
-                as: 'cardtype',
-            },
-        },
-        {
-            $unwind: {
-                path: '$cardtype',
-                preserveNullAndEmptyArrays: true,
-            },
-        },
-
-        {
-            $group: {
-                _id: '$_id',
-                root: { $mergeObjects: '$$ROOT' },
-                cardtype: { $first: '$cardtype' },
-            },
-        },
-        {
-            $replaceRoot: {
-                newRoot: {
-                    $mergeObjects: ['$root', '$$ROOT'],
-                },
-            },
-        },
-        {
-            $project: {
-                root: 0,
-                __v: 0,
-                'cardtype.__v': 0,
-                active: 0,
-                'cardtype.active': 0,
-            },
-        },
-    ])
-        .sort({ createdAt: -1 })
-        .then(data => {
-            return res.status(200).send({
-                success: true,
-                message: 'Cards retrieved successfully',
-                count: data.length,
-                data,
-            });
-        })
-        .catch(err => {
-            return next({
-                status: 500,
-                success: false,
-                message: 'Error fetching cards',
-                detailed_message: err.message,
-            });
-        });
+    return next(await cardListDetailedQuery());
 };
 
 const createCard = async (req, res, next) => {
-    const { name, user_id, card_type_id, url_path } = req.body;
-    const data = new Card({
-        name,
-        user_id,
-        card_type_id,
-        url_path,
-    });
-    await data
-        .save()
-        .then(data => {
-            return res.status(200).send({
-                success: true,
-                message: 'Card created successfully',
-                data,
-            });
-        })
-        .catch(err => {
-            return next({
-                status: 500,
-                success: false,
-                message: 'Error creating card',
-                detailed_message: err.message,
-            });
-        });
+    return next(await cardCreateQuery(req.body));
 };
 
 const getCard = async (req, res, next) => {
     const { id } = req.params;
-    await Card.findById({ _id: id }, { __v: 0 })
-        .then(data => {
-            if (!data) {
-                return res.status(200).send({
-                    success: false,
-                    message: 'Card not found',
-                    data: null,
-                });
-            } else {
-                return res.status(200).send({
-                    success: true,
-                    message: 'Card retrieved successfully',
-                    data,
-                });
-            }
-        })
-        .catch(err => {
-            return next({
-                status: 500,
-                success: false,
-                message: 'Error fetching card',
-                detailed_message: err.message,
-            });
-        });
+    return next(await cardGetQuery(id));
 };
 
 const deleteCard = async (req, res, next) => {
     const { id } = req.params;
-    await Card.findOneAndDelete(id)
-        .then(data => {
-            if (!data) {
-                return res.status(200).send({
-                    success: false,
-                    message: 'Card not found',
-                    data: null,
-                });
-            } else {
-                return res.status(200).send({
-                    success: true,
-                    message: 'Card deleted successfully',
-                    data,
-                });
-            }
-        })
-        .catch(err => {
-            return next({
-                status: 500,
-                success: false,
-                message: 'Error deleting card',
-                detailed_message: err.message,
-            });
-        });
+    return next(await cardDeleteQuery(id));
 };
 
 export { listCards, listDetailedCards, createCard, getCard, deleteCard };
---
+++ src/modules/Card/card-query.js
@@ -0,0 +1,171 @@
+import Card from './card-model.js';
+
+const cardListQuery = async () => {
+    return await Card.find({}, { __v: 0 })
+        .sort({ createdAt: -1 })
+        .then(data => {
+            return {
+                status: 200,
+                success: true,
+                message: 'Cards retrieved successfully',
+                count: data.length,
+                data,
+            };
+        })
+        .catch(err => {
+            return {
+                status: 500,
+                success: false,
+                message: 'Error fetching cards',
+                detailed_message: err.message,
+            };
+        });
+};
+
+const cardListDetailedQuery = async () => {
+    await Card.aggregate([
+        {
+            $lookup: {
+                from: 'cardtypes',
+                localField: 'card_type_id',
+                foreignField: '_id',
+                as: 'cardtype',
+            },
+        },
+        {
+            $unwind: {
+                path: '$cardtype',
+                preserveNullAndEmptyArrays: true,
+            },
+        },
+
+        {
+            $group: {
+                _id: '$_id',
+                root: { $mergeObjects: '$$ROOT' },
+                cardtype: { $first: '$cardtype' },
+            },
+        },
+        {
+            $replaceRoot: {
+                newRoot: {
+                    $mergeObjects: ['$root', '$$ROOT'],
+                },
+            },
+        },
+        {
+            $project: {
+                root: 0,
+                __v: 0,
+                'cardtype.__v': 0,
+                active: 0,
+                'cardtype.active': 0,
+            },
+        },
+    ])
+        .sort({ createdAt: -1 })
+        .then(data => {
+            return {
+                status: 200,
+                success: true,
+                message: 'Cards retrieved successfully',
+                count: data.length,
+                data,
+            };
+        })
+        .catch(err => {
+            return {
+                status: 500,
+                success: false,
+                message: 'Error fetching cards',
+                detailed_message: err.message,
+            };
+        });
+};
+
+const cardCreateQuery = async body => {
+    const { name, user_id, card_type_id, url_path } = body;
+    return await new Card({
+        name,
+        user_id,
+        card_type_id,
+        url_path,
+    })
+        .save()
+        .then(data => {
+            return {
+                status: 200,
+                success: true,
+                message: 'Card created successfully',
+                data,
+            };
+        })
+        .catch(err => {
+            return {
+                status: 500,
+                success: false,
+                message: 'Error creating card',
+                detailed_message: err.message,
+            };
+        });
+};
+
+const cardGetQuery = async id => {
+    return await Card.findById({ _id: id }, { __v: 0 })
+        .then(data => {
+            if (!data) {
+                return {
+                    status: 200,
+                    success: false,
+                    message: 'Card not found',
+                    data: null,
+                };
+            } else {
+                return {
+                    status: 200,
+                    success: true,
+                    message: 'Card retrieved successfully',
+                    data,
+                };
+            }
+        })
+        .catch(err => {
+            return {
+                status: 500,
+                success: false,
+                message: 'Error fetching card',
+                detailed_message: err.message,
+            };
+        });
+};
+
+const cardDeleteQuery = async id => {
+    return await Card.findOneAndDelete(id)
+        .then(data => {
+            if (!data) {
+                return {
+                    status: 200,
+                    success: false,
+                    message: 'Card not found',
+                    data: null,
+                };
+            } else {
+                return {
+                    status: 200,
+                    success: true,
+                    message: 'Card deleted successfully',
+                    data,
+                };
+            }
+        })
+        .catch(err => {
+            return {
+                status: 500,
+                success: false,
+                message: 'Error deleting card',
+                detailed_message: err.message,
+            };
+        });
+};
+
+export { cardListQuery, cardListDetailedQuery, cardCreateQuery, cardGetQuery, cardDeleteQuery };
