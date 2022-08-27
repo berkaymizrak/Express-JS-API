4f080dac306b627f63a0f9d63f9c546984ffc573
Berkay MIZRAK <mizrakby@gmail.com>
2022-08-27 11:46:54 +0300

    example .env file is added

--- src/config.js
+++ src/config.js
@@ -4,17 +4,18 @@ dotenv.config();
 
 const mongoUri = `${process.env.DB_HOST}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?${process.env.DB_CLUSTER_CONFIG}`;
 
-const port = process.env.PORT || 8080;
-const JWT_SECRET = process.env.JWT_SECRET;
-const JWT_ALGORITHM = process.env.JWT_ALGORITHM;
-const JWT_REFRESH_ALGORITHM = process.env.JWT_REFRESH_ALGORITHM;
+const port = process.env.PORT || 3001;
+const JWT_SECRET = process.env.JWT_SECRET || 'secret';
+const JWT_ALGORITHM = process.env.JWT_ALGORITHM || 'HS512';
+const JWT_REFRESH_ALGORITHM = process.env.JWT_REFRESH_ALGORITHM || 'HS256';
 
-// You may use this as a boolean value for different situations
+// You may use this as a boolean value for different situations from 'env' object below.
+const NODE_ENV = process.env.NODE_ENV || 'development';
 const env = {
-    development: process.env.NODE_ENV === 'development',
-    test: process.env.NODE_ENV === 'test',
-    staging: process.env.NODE_ENV === 'staging',
-    production: process.env.NODE_ENV === 'production',
+    development: NODE_ENV === 'development',
+    test: NODE_ENV === 'test',
+    staging: NODE_ENV === 'staging',
+    production: NODE_ENV === 'production',
 };
 
 export { mongoUri, port, JWT_SECRET, JWT_ALGORITHM, JWT_REFRESH_ALGORITHM, env };
