52a6a351fb7a6190ec5291171f94817e7d20c0fa
Berkay MIZRAK <mizrakby@gmail.com>
2022-09-14 18:40:48 +0300

    production settings

--- src/modules/Admin/admin-router.js
+++ src/modules/Admin/admin-router.js
@@ -1,11 +1,39 @@
 import AdminJSExpress from '@adminjs/express';
 import { userFindQuery } from '../User/user-query.js';
-import adminAuth from '../../middlewares/admin-auth.js';
+// import adminAuth from '../../middlewares/admin-auth.js';
 import { sessionOptions } from '../../config.js';
-import adminJs from './admin-config.js';
+// import adminJs from './admin-config.js';
+import AdminJS from 'adminjs';
+import AdminJSMongoose from '@adminjs/mongoose';
+import dbConnection from '../../services/db.js';
+
+AdminJS.registerAdapter(AdminJSMongoose);
+const mongooseDb = await dbConnection();
+// Full doc: https://docs.adminjs.co/tutorial-customizing-resources.html
+const adminJs = await new AdminJS({
+    databases: [mongooseDb],
+    resources: [],
+    rootPath: '/api/admin',
+    loginPath: '/api/admin/login',
+    logoutPath: '/api/admin/logout',
+    branding: {
+        companyName: 'Express API Admin',
+        withMadeWithLove: false,
+        logo: '/static/images/logo.svg',
+    },
+    locale: {
+        translations: {
+            labels: {
+                User: 'Users xx',
+                Card: 'Cards xx',
+                CardType: 'Card Typesx x',
+            },
+        },
+    },
+});
 
 const adminRouter = await AdminJSExpress.buildAuthenticatedRouter(
-    adminJs,
+    await adminJs,
     {
         authenticate: async (email, password) => {
             const filters = { email };
@@ -30,7 +58,7 @@ const adminRouter = await AdminJSExpress.buildAuthenticatedRouter(
             );
         },
     },
-    adminAuth,
+    null,
     sessionOptions
 );
 
