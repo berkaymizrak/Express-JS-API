e2dc18cd4262986c6bef12b387a301db82cb08f9
Berkay MIZRAK <mizrakby@gmail.com>
2022-09-15 13:34:41 +0300

    refactor: convert middleware to function

--- src/middlewares/admin-required.js
+++ src/middlewares/admin-required.js
@@ -1,13 +1,9 @@
-import express from 'express';
-
-const adminRequired = express.Router();
-
-adminRequired.use((req, res, next) => {
+const adminRequired = (req, res, next) => {
     if ((req.session && req.session.adminUser) || (req.session.user && req.session.user.role === 'admin')) {
         return next();
     } else {
         return next({ success: false, status: 401, mes: 'Unauthorized to access this resource' });
     }
-});
+};
 
 export default adminRequired;
