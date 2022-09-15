1deae02bd0b263af58b5d2c43651a72201080f8a
Berkay MIZRAK <mizrakby@gmail.com>
2022-09-15 13:58:18 +0300

    feat: admin login with username OR email

--- src/modules/Admin/admin-router.js
+++ src/modules/Admin/admin-router.js
@@ -1,35 +1,49 @@
 import AdminJSExpress from '@adminjs/express';
 import { userFindQuery } from '../User/user-query.js';
 import adminAuth from '../../middlewares/admin-auth.js';
-import { sessionOptions, cookiePassword } from '../../config.js';
+import { sessionOptions, cookiePassword, logger } from '../../config.js';
 import adminJs from './admin-config.js';
+import isEmailValid from '../../services/emailValidator.js';
 
 const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
     adminJs,
     {
-        authenticate: async (email, password) => {
-            const filters = { email };
-            return await userFindQuery({}, { filters, limit: 1, projection: {} }).then(
-                async responseUserFindQuery => {
-                    const { data } = responseUserFindQuery;
-                    if (
-                        responseUserFindQuery.success &&
-                        data &&
-                        data.length > 0 &&
-                        data[0].schema.methods.isAdmin(data[0].role)
-                    ) {
-                        const user = data[0];
-
-                        return await user.comparePassword(password).then(async isMatch => {
-                            if (isMatch.success) {
-                                return user;
-                            }
-                        });
+        authenticate: async (usernameOrEmail, password) => {
+            return await isEmailValid(usernameOrEmail)
+                .then(async isEmail => {
+                    const filters = {};
+                    if (isEmail) {
+                        filters.email = usernameOrEmail;
+                    } else {
+                        filters.username = usernameOrEmail;
                     }
-                }
-            );
+
+                    return await userFindQuery({}, { filters, limit: 1, projection: {} }).then(
+                        async responseUserFindQuery => {
+                            const { data } = responseUserFindQuery;
+                            if (
+                                responseUserFindQuery.success &&
+                                data &&
+                                data.length > 0 &&
+                                data[0].schema.methods.isAdmin(data[0].role)
+                            ) {
+                                const user = data[0];
+
+                                return await user.comparePassword(password).then(async isMatch => {
+                                    if (isMatch.success) {
+                                        return user;
+                                    }
+                                });
+                            }
+                        }
+                    );
+                })
+                .catch(err => {
+                    logger.error('Admin login error:', err);
+                    return;
+                });
         },
-        cookiePassword: cookiePassword,
+        cookiePassword,
     },
     adminAuth,
     sessionOptions
