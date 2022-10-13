a3d80d685ab269f5e2d661d71d7de5870b84ba98
Berkay MIZRAK <mizrakby@gmail.com>
2022-10-14 00:46:59 +0300

    refactor: change reset password done method to 'post'

--- src/modules/Auth/auth-router.js
+++ src/modules/Auth/auth-router.js
@@ -16,7 +16,7 @@ authRouter
     .post('/login', loginUser)
     .post('/reset_password_request', resetPasswordRequest)
     .get('/reset_password_confirm', resetPasswordConfirm)
-    .get('/reset_password_done', resetPasswordDone)
+    .post('/reset_password_done', resetPasswordDone)
     .get('/tokens', setPaginationParams, listTokens);
 
 export default authRouter;
